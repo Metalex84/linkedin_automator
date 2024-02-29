186329cd961787323b9be6d653cd994214715d54
Metalex84 <agonzalez.venegas@outlook.com>
2024-02-29 21:38:44 +0100

    OK el array de resultados, pero falta la paginacion

--- app.py
+++ app.py
@@ -8,18 +8,20 @@ import random
 import time
 
 app = Flask(__name__)
+
+app.debug = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 current_year = datetime.now().year
 app.config['opcion'] = None
-
-driver = webdriver.Chrome()
+app.config['driver'] = None
 
 @app.route('/')
 def index():
     session.clear()
+    app.config['driver'] = webdriver.Chrome()
     return render_template('index.html', current_year=current_year)
 
 
@@ -43,14 +45,14 @@ def login():
     usuario = request.form.get('username')
     contrasena = request.form.get('password')
     # Abro el navegador con LinkedIn
-    driver.get('https://www.linkedin.com')
+    app.config['driver'].get('https://www.linkedin.com')
     # Busco los campos de usuario y contraseña para meter los mios
-    username = driver.find_element(By.XPATH, '//*[@id="session_key"]')
-    password = driver.find_element(By.XPATH, '//*[@id="session_password"]')
+    username = app.config['driver'].find_element(By.XPATH, '//*[@id="session_key"]')
+    password = app.config['driver'].find_element(By.XPATH, '//*[@id="session_password"]')
     username.send_keys(usuario)
     password.send_keys(contrasena)
     # Busco el boton de submit para enviar el formulario web
-    submit = driver.find_element(By.XPATH, '//button[@type="submit"]')
+    submit = app.config['driver'].find_element(By.XPATH, '//button[@type="submit"]')
     submit.click()
     time.sleep(random.randint(1, 4))
     return render_template('busqueda.html', usuario=usuario, current_year=current_year)
@@ -58,27 +60,31 @@ def login():
 
 @app.route('/busqueda', methods=['POST', 'GET'])
 def busqueda():
-
-    texto = request.form.get('texto_busqueda')
-    driver.get(f"https://www.linkedin.com/search/results/people/?keywords={texto}&origin=SWITCH_SEARCH_VERTICAL")
     time.sleep(random.randint(1, 4))
-    # OJO: empezar con la paginación! Averiguar cuántas páginas de resultados se producen 
     # OJO: implementar opcion de busqueda avanzada: contactos de 2 o 3 grado, ubicacion, 
     opt = app.config['opcion']
     if opt == '1':
-        profiles_pattern = 'app-aware-link '
-        profiles = driver.find_elements(By.XPATH, f'//*[@class="{profiles_pattern}"]') # and @tabindex="-1"]')
+        # Voy a intentar averiguar cuántas páginas de resultados se producen. Me lo dirá el contenido del último button de la 'ul'...
+        '''
+        ESTO TODAVÍA NO SALE
+        '''
+        pagina = 1
+        lista_raiz = app.config['driver'].find_elements(By.XPATH, '//*[@class="artdeco-pagination__pages artdeco-pagination__pages--number"]')
+        print(lista_raiz)
+   
+
+        app.config['driver'].get(f"https://www.linkedin.com/search/results/people/?keywords={request.form.get('texto_busqueda')}&origin=SWITCH_SEARCH_VERTICAL&page={pagina}")
+        profiles = app.config['driver'].find_elements(By.XPATH, '//*[@class="app-aware-link  scale-down "]')
         visit_profiles = [p for p in profiles]
         # OJO: copiar el contenido de visit_profiles a una lista nueva para que no se pierda al cerrar el navegador
         for p in visit_profiles:
             p_url = p.get_attribute('href')
-            driver.execute_script(f"window.open('{p_url}');")
+            app.config['driver'].execute_script(f"window.open('{p_url}');")
             # En cada iteracion se abre una nueva pestaña con el perfil de la persona pero con webdriver, no con JS
             # Utiliza otro webdriver: driver2.get(p_url)
             # OJO: implementar un tiempo aleatorio para que no se bloquee el navegador
-            # driver.close()
+            # app.config['driver'].close()
             time.sleep(random.randint(1, 4))
-        # OJO: implementar un tiempo aleatorio para que no se bloquee el navegador
     
     elif opt == '2':
         print('Escribir mensajes no implementado aun')
@@ -90,7 +96,7 @@ def busqueda():
     else:
         return render_template('index.html', current_year=current_year)
 
-    driver.close()
+    app.config['driver'].close()
     session.clear()
     return render_template("done.html", profiles=visit_profiles, current_year=current_year)
 
@@ -98,7 +104,7 @@ def busqueda():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 
     '''
